50ce68e1fdacb162935653309c994ab470702f15
Tbernard33 <tbernard@lucine.io>
2022-02-22 15:34:12 +0100

    Functionning Post request : Raw

---
+++ Assets/Scripts/JsonWheel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Turn the JsonFile into a "JsonWheel" wich can now be easily accessed in c# with every parameter readable
+/// </summary>
+[System.Serializable]
+public class JsonWheel
+{
+    public int index;
+    public int euros;
+    public bool isJackpot;
+
+    public static JsonWheel CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<JsonWheel>(jsonString);
+    }
+
+    public JsonWheel()
+    {
+        index = 0;
+        euros = 0;
+        isJackpot = false;
+    }
+}
